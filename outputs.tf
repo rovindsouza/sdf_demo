5619990198aba5b2f4e01781f78686dc49ea59c5
ATC Team <atcteam@google.com>
2022-09-20 09:18:27 +0000

    This is being added from ATC's SDF

--- outputs.tf
+++ outputs.tf
@@ -38,7 +38,6 @@ output "ca_certificate" {
   description = "The cluster ca certificate (base64 encoded)"
   value       = module.gke.ca_certificate
   sensitive   = true
-
 }
 
 output "service_account" {
